e2115d165526db9eceaeb554947cda33c944b555
Stanley Miracle <stanleyxmiracle@gmail.com>
2022-03-17 19:48:50 -0400

    update

--- articles/js/article.js
+++ articles/js/article.js
@@ -1,34 +1,32 @@
 function invertColor(source) {
     var chkbox = document.getElementById(source);
     if (chkbox.checked) { // night
-        document.body.classList.add("reader-night-mode");
-        var elms = document.getElementsByClassName('tooltiptext');
+        document.body.classList.add('reader-night-mode');
+        var elms = document.getElementsByTagName('abbr');
         for (let i = 0; i < elms.length; i++) {
             const elm = elms[i];
-            elm.classList.add("reader-day-mode");
-            elm.classList.remove("reader-night-mode");
+            elm.style.borderBottom = '1px dashed #fff';
         }
     }
     else { // day
-        document.body.classList.remove("reader-night-mode");
-        var elms = document.getElementsByClassName('tooltiptext');
+        document.body.classList.remove('reader-night-mode');
+        var elms = document.getElementsByTagName('abbr');
         for (let i = 0; i < elms.length; i++) {
             const elm = elms[i];
-            elm.classList.add("reader-night-mode");
-            elm.classList.remove("reader-day-mode");
-        }        
+            elm.style.borderBottom = '1px dashed #c8c8c8';
+        }       
     }
 }
 
-document.body.addEventListener('click', function(e) {
-    var elms = document.getElementsByClassName('tooltip');
-    for (let i = 0; i < elms.length; i++) {
-        const elm = elms[i];
-        if (e.target == elm) {
-            elm.children[0].style.visibility = 'visible';
-        }
-        else {
-            elm.children[0].style.visibility = 'hidden';
-        }
-    }
-});
+// document.body.addEventListener('click', function(e) {
+//     var elms = document.getElementsByClassName('tooltip');
+//     for (let i = 0; i < elms.length; i++) {
+//         const elm = elms[i];
+//         if (e.target == elm) {
+//             elm.children[0].style.visibility = 'visible';
+//         }
+//         else {
+//             elm.children[0].style.visibility = 'hidden';
+//         }
+//     }
+// });
