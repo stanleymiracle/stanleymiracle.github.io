e989669b48c75481cebb2f952386399bcd48e920
Stanley Miracle <stanleyxmiracle@gmail.com>
2022-03-24 23:23:52 -0400

    update

--- articles/js/article.js
+++ articles/js/article.js
@@ -2,20 +2,36 @@ function invertColor(source) {
     var chkbox = document.getElementById(source);
     if (chkbox.checked) { // night
         document.body.classList.add('reader-night-mode');
-        var elms = document.getElementsByTagName('abbr');
+        setClassBorderBottom('abbr', true);
+        setClassBorderBottom('text-to-img', true);
+    }
+    else { // day
+        document.body.classList.remove('reader-night-mode');
+        setClassBorderBottom('abbr', false);      
+        setClassBorderBottom('text-to-img', false);      
+    }
+}
+
+function setClassBorderBottom(className, isNight) {
+    if (isNight) {
+        var elms = document.getElementsByClassName(className);
         for (let i = 0; i < elms.length; i++) {
             const elm = elms[i];
             elm.style.borderBottom = '1px dashed #fff';
         }
     }
-    else { // day
-        document.body.classList.remove('reader-night-mode');
-        var elms = document.getElementsByTagName('abbr');
+    else {
+        var elms = document.getElementsByClassName(className);
         for (let i = 0; i < elms.length; i++) {
             const elm = elms[i];
             elm.style.borderBottom = '1px dashed #c8c8c8';
-        }       
+        }
     }
+}
+
+function addImageBackgroundToModal(path) {
+    var elm = document.getElementById('modal-img');
+    elm.src = path;
 }
 
 (function(){
@@ -23,6 +39,17 @@ function invertColor(source) {
     if (today.getHours() > 18) {
         document.getElementById('toggle').click();
     }
+    var ttis = document.getElementsByClassName('text-to-img');
+    for (let i = 0; i < ttis.length; i++) {
+        const elm = ttis[i];
+        elm.onclick = function() {
+            addImageBackgroundToModal(elm.getAttribute('data-path'));
+            document.getElementById('page-modal').style.display = 'block';
+        };        
+    }
+    document.getElementById('modal-close').onclick = function() {
+        document.getElementById('page-modal').style.display = 'none';
+    };
 })();
 
 // document.body.addEventListener('click', function(e) {
