0c215eb6f96a8c73e156499a19171da70a9e8b83
Stanley Miracle <stanleyxmiracle@gmail.com>
2022-06-06 11:32:29 -0400

    update

---
+++ blogs/shredderyin/js/fun.js
@@ -0,0 +1,54 @@
+
+function nightOn() {
+    var tab = document.getElementsByClassName("tab-item");
+    for (var i = 0; i < tab.length; i++) {
+        tab[i].style.color = "grey";
+    }
+    var bar = document.getElementsByClassName("navbar-default");
+    for (var i = 0; i < bar.length; i++) {
+        bar[i].style.backgroundColor = "#000";
+    }
+    var title = document.getElementsByClassName("title");
+    for (var i = 0; i < title.length; i++) {
+        title[i].style.backgroundColor = "#000";
+    }
+    document.body.style.backgroundColor = "#000";
+}
+
+function openTab(tabName) {
+    var item = document.getElementsByClassName("tab-item");
+    for (var i = 0; i < item.length; i++) {
+        item[i].style.border = "none";
+    }
+    document.getElementById(tabName + "-tab").style.border = "1px solid #fff";
+
+    var list = document.getElementsByClassName("list-group-item");
+    for (var i = 0; i < list.length; i++) {
+        list[i].style.display = "none";
+    }
+
+    var tab = document.getElementsByClassName(tabName);
+    for (var i = 0; i < tab.length; i++) {
+        tab[i].style.display = "block";
+    }
+}
+
+function sortOn() {
+    document.getElementById("tab-bar").style.display = "block";
+    document.getElementById("sort-on").style.display = "none";
+    document.getElementById("sort-off").style.display = "block";
+}
+
+function sortOff() {
+    document.getElementById("tab-bar").style.display = "none";
+    document.getElementById("sort-on").style.display = "block";
+    document.getElementById("sort-off").style.display = "none";
+    var item = document.getElementsByClassName("tab-item");
+    for (var i = 0; i < item.length; i++) {
+        item[i].style.backgroundColor = "";
+    }
+    var list = document.getElementsByClassName("list-group-item");
+    for (var i = 0; i < list.length; i++) {
+        list[i].style.display = "block";
+    }
+}
