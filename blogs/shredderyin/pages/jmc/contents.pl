c7fc5383d36121ea96b5b431016a0554dd2f6827
Stanley Miracle <stanleyxmiracle@gmail.com>
2022-06-06 20:52:28 -0400

    update

---
+++ blogs/shredderyin/pages/jmc/contents.pl
@@ -0,0 +1,43 @@
+# LaTeX2HTML 96.1 (Feb 5, 1996)
+# Associate contents original text with physical files.
+
+$key = q/0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '0%:%basis1.html%:%A BASIS FOR A MATHEMATICAL THEORY OF COMPUTATION ' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%node1.html%:%Introduction' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%node2.html%:%Formalisms For Describing Computable Functions and Related Entities' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 2 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%node3.html%:%Functions Computable in Terms of Given Base Functions ' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%node9.html%:%Properties of Computable Functions' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 2 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%node4.html%:%Recursive Functions of the Integers' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 3 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%node10.html%:%Formal Properties of Conditional Forms' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%node12.html%:%Relation to Other Formalisms' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 2 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%node5.html%:%Computable Functionals' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 3 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%node11.html%:%Recursion Induction' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 4 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%node13.html%:%Recursive function theory' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%node15.html%:%Conclusion: Mathematical Theory of Computation' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 2 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%node6.html%:%Non-Computable Functions and Functionals' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 4 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%node14.html%:%On the Relations between Computation and Mathematical
+  Logic' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 6 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%node16.html%:%REFERENCES' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 2 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%node7.html%:%Ambiguous Functions' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 7 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%node17.html%:%  About this document ... ' unless ($toc_section_info{$key}); 
+$key = q/0 0 0 2 6 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%node8.html%:%Recursive Definitions of Sets' unless ($toc_section_info{$key}); 
+
+1;
+
---
+++ blogs/shredderyin/pages/jmc/sections.pl
@@ -0,0 +1,61 @@
+# LaTeX2HTML 96.1 (Feb 5, 1996)
+# Associate sections original text with physical files.
+
+$key = q/0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '0%:%basis1.html%:%A BASIS FOR A MATHEMATICAL THEORY OF COMPUTATION ' unless ($section_info{$key}); 
+$done{"basis1.html"} = 1;
+$key = q/0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%node1.html%:%Introduction' unless ($section_info{$key}); 
+$done{"node1.html"} = 1;
+$key = q/0 0 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%node2.html%:%Formalisms For Describing Computable Functions and Related Entities' unless ($section_info{$key}); 
+$done{"node2.html"} = 1;
+$key = q/0 0 0 2 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '4%:%node3.html%:%Functions Computable in Terms of Given Base Functions ' unless ($section_info{$key}); 
+$done{"node3.html"} = 1;
+$key = q/0 0 0 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%node9.html%:%Properties of Computable Functions' unless ($section_info{$key}); 
+$done{"node9.html"} = 1;
+$key = q/0 0 0 2 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '4%:%node4.html%:%Recursive Functions of the Integers' unless ($section_info{$key}); 
+$done{"node4.html"} = 1;
+$key = q/0 0 0 3 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '4%:%node10.html%:%Formal Properties of Conditional Forms' unless ($section_info{$key}); 
+$done{"node10.html"} = 1;
+$key = q/0 0 0 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%node12.html%:%Relation to Other Formalisms' unless ($section_info{$key}); 
+$done{"node12.html"} = 1;
+$key = q/0 0 0 2 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '4%:%node5.html%:%Computable Functionals' unless ($section_info{$key}); 
+$done{"node5.html"} = 1;
+$key = q/0 0 0 3 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '4%:%node11.html%:%Recursion Induction' unless ($section_info{$key}); 
+$done{"node11.html"} = 1;
+$key = q/0 0 0 4 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '4%:%node13.html%:%Recursive function theory' unless ($section_info{$key}); 
+$done{"node13.html"} = 1;
+$key = q/0 0 0 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%node15.html%:%Conclusion: Mathematical Theory of Computation' unless ($section_info{$key}); 
+$done{"node15.html"} = 1;
+$key = q/0 0 0 2 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '4%:%node6.html%:%Non-Computable Functions and Functionals' unless ($section_info{$key}); 
+$done{"node6.html"} = 1;
+$key = q/0 0 0 4 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '4%:%node14.html%:%On the Relations between Computation and Mathematical
+  Logic' unless ($section_info{$key}); 
+$done{"node14.html"} = 1;
+$key = q/0 0 0 6 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%node16.html%:%REFERENCES' unless ($section_info{$key}); 
+$done{"node16.html"} = 1;
+$key = q/0 0 0 2 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '4%:%node7.html%:%Ambiguous Functions' unless ($section_info{$key}); 
+$done{"node7.html"} = 1;
+$key = q/0 0 0 7 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '3%:%node17.html%:%  About this document ... ' unless ($section_info{$key}); 
+$done{"node17.html"} = 1;
+$key = q/0 0 0 2 6 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$section_info{$key} = '4%:%node8.html%:%Recursive Definitions of Sets' unless ($section_info{$key}); 
+$done{"node8.html"} = 1;
+
+1;
+
